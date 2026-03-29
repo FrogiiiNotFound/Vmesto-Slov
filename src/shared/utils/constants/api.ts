13efe0f59be9573e45004d8cbd21b6161579df53
Арсен <samsung2903166@gmail.com>
2026-03-29 10:27:28 +0300

    small change

--- src/shared/utils/constants/api.ts
+++ src/shared/utils/constants/api.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "https://vmesto-slov-api.onrender.com";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "https://vmesto-slov-api.onrender.com/api/v1";
 export const MOCK_API_BASE_URL =
     "https://6913091152a60f10c823c1a7.mockapi.io/api/v2/";
